507c25c1cb4d29413c15307be781f556343ea578
PHE <phephe8236@gmail.com>
2022-11-30 00:29:27 +0900

    first commit

---
+++ src/main/java/hello/pheboard/PheboardApplication.java
@@ -0,0 +1,13 @@
+package hello.pheboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PheboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PheboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/pheboard/PheboardApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.pheboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PheboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
